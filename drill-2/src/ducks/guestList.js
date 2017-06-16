ac39025af534c71c89a0ecacf6d8f23512352108
loren pabst <loren pabst>
2017-06-16 11:18:03 -0600

    drill 2 is done and i added redux and react-redux to all of the package.json files

--- drill-2/src/ducks/guestList.js
+++ drill-2/src/ducks/guestList.js
@@ -1,1 +1,16 @@
 
+const initialState = [
+    'Tony Stark', 
+    'Steve Rodgers', 
+    ' Nick Fury', 
+    'Natasha Romanova', 
+    'Clint Barton', 
+    'Bruce Banner', 
+    'Wanda Maximoff'
+    ];
+
+function reducer (state = initialState, action){
+    return state;
+}
+
+export default reducer;
